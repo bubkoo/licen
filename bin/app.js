ae55d5d50eac1a23f5d6698c999143a965f91305
bubkoo <bubkoo@163.com>
2016-03-13 18:25:48 +0800

    temp

---
+++ bin/app.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+var pkg       = require('../package.json');
+var parseArgs = require('minimist');
+var promptFor = require('../lib/prompts');
+
+
+var version = 'v' + pkg.version;
+
+var argv = parseArgs(process.argv.slice(2), {
+  alias  : {
+    h: 'help',
+    V: 'version',
+    l: 'list',
+    y: 'year',
+    u: ['fullname', 'user', 'username', 'user-name', 'full-name',],
+    e: 'email'
+  },
+  default: {},
+  boolean: [],
+  string : [],
+  unknown: function (name) {
+    console.log('  Unknown option: ', name);
+  }
+});
+
+
+if (argv.help) {
+  return;
+}
+
+
+if (argv.version) {
+  return console.log(version);
+}
+
+
+promptFor.action(argv);
